d95b3cdd84923be0f88304fd33514b9f71028c61
Fred <fredericogon@gmail.com>
2022-02-06 11:58:11 +0000

    initial commit

---
+++ src/main/java/com/store/fabulous/FabulousApplication.java
@@ -0,0 +1,13 @@
+package com.store.fabulous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabulousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabulousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/store/fabulous/FabulousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.fabulous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabulousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
